d12f42b8f8f8012630954a6e0b323db15d394218
asp-myuser <akashsavaliya2710@gmail.com>
2022-04-01 04:42:04 -0400

    generate_cuts first draft

--- discrete.py
+++ discrete.py
@@ -6,7 +6,7 @@ class Discretize():
 
     def __init__(self):
         self.initial_cuts = []
-        self.final_cuts = []
+        self.final_cuts = {}
         self.df = None
         self.attributes = []
         self.dec = None
@@ -30,7 +30,7 @@ class Discretize():
         print("Writing data at ",path)
 
     def transform(self):
-        print("Sarting transformation")
+        print("Starting transformation")
         self.__generate_cuts()
         self.__transform_df()
         print("Transformation complete")
@@ -41,12 +41,55 @@ class Discretize():
         The cuts are generated to discretize the table using the discernibilty formulas algorithm.
         """
         print("Generating cuts")
+        self.attributes = list(self.df.columns)[:-1]
+        self.dec = list(self.df.columns)[-1]
 
-    
+        labels = list(self.df[self.dec].unique())
+        indices_to_pair = [ self.df.index[ self.df[self.dec] == label ].tolist() for label in labels ]
+        
+        pairs = []
+        for i in range(len(indices_to_pair)-1):
+            for j in range(i+1,len(indices_to_pair)):
+                pairs.extend( list( itertools.product( indices_to_pair[i], indices_to_pair[j] ) ) )
+        
+        cuts = {}
+        for index,attri in enumerate(self.attributes):
+            val = sorted(list(self.df[attri].unique()))
+            for i in range(1,len(val)):
+                cut = (val[i-1] + val[i])/2
+                self.initial_cuts.append( (index, cut) )
+                if index not in cuts:
+                    cuts[index] = []
+                cuts[index].append(cut)
+                
+        rows = dict.fromkeys(pairs,set())
+        cols = dict.fromkeys(self.initial_cuts,set())
+
+        for pair in pairs:
+            x1, x2 = pair
+            for index,attri in enumerate(self.attributes):
+                for cut in cuts[index]:
+                    if self.df.loc[x1, attri] < cut < self.df.loc[x2, attri] or self.df.loc[x2, attri] < cut < self.df.loc[x1, attri] :
+                        rows[pair].add( (index, cut) )
+                        cols[(index, cut)].add(pair)
+        
+        while bool(rows):
+            counts =[(len(value), key[0], key[1]) for key,value in cols.items()]
+            optimal_cut = sorted(counts, key = lambda x: (-x[0],x[1],x[2]))[0][1:]
+            discern_rows = cols[optimal_cut]
+            for col in cols.keys():
+                cols[col].difference_update(discern_rows)
+            for row in discern_rows:
+                del rows[row]
+            if optimal_cut[0] not in self.final_cuts:
+                self.final_cuts[ optimal_cut[0] ] = []
+            self.final_cuts[ optimal_cut[0] ].append(optimal_cut[1])
+
     def __transform_df(self):
         """
         This private function/method takes in no parameter.
         The df dataframe is transformed and stored into the trans_df using the final cuts list.
+        eg : self.final_cuts = {0: [0.9, 1.5], 1: [0.75, 1.5] } where key is the attribute index and value is list of cuts
         """
         print("Transforming dataframe")
 
